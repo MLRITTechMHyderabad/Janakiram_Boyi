6c95bc1a45e3f14035d779930d3a8be0fcaf3751
JANAKIRAM421 <121026951+JANAKIRAM421@users.noreply.github.com>
2025-04-19 09:38:29 +0530

    Delete Project directory

--- Project/D_configure.py
+++
@@ -1,5 +0,0 @@
-DATA_PATH = r"C:\Users\janak\Downloads\Global_Cybersecurity_Threats_2015-2024.csv"
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = 'root'
-MYSQL_DB = 'CyberSecurity'
